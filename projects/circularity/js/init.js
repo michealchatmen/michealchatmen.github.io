3fd8ab6349551a9f3356fe03f74397a468222029
michealchatmen <chatman52405@students.apsb.org>
2021-05-11 16:25:46 +0000

    adding circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,12 +20,27 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
+        var circle;		//variable to hold a single circle when creating circles/iterating
+var circles = [];	//variable to store all circles in one Array
 
 
-        // TODO 2 : Create a function that draws a circle 
+
+        // TODO 2 : Create a function that draws a circle
+        function drawCircle() {
+//code to draw a circle
+	circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+	physikz.addRandomVelocity(circle, canvas);
+	view.addChild(circle);
+	circles.push(circle);
+}
+ 
         
 
         // TODO 3 / 7 : Call the drawCircle() function 
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
+	drawCircle();
+}
+
 
 
         ////////////////////////////////////////////////////////////
@@ -39,13 +54,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+          
             
             // TODO 5 : Call game.checkCirclePosition() on your circles.
            
 
             // TODO 8 : Iterate over the array
-           
+           for (var i = 0; i < circles.length; i++) {
+                var eachCircle = circles[i];   
+                physikz.updatePosition(eachCircle);
+                game.checkCirclePosition(eachCircle);           
+            }
+
             
         }
     
@@ -62,6 +82,16 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
+            if (circle.x < 0) {
+		circle.x = canvas.width;
+	}
+	if (circle.y > canvas.width) {
+		circle.y = 0;
+	}
+	if (circle.y < 0) {
+		circle.y = canvas.height;
+	}
+
             
 
 
